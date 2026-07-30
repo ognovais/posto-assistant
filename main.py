9fa82e337cd656e9e1d24005f30642b246d7c005
Antony Novais Nunes <159445979+ognovais@users.noreply.github.com>
2026-07-30 08:30:17 -0300

    Add button to edit CNPJ in main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton
 from modules.ativacao.janela import abrir_janela_ativacao
 from modules.carga_full.janela import abrir_janela_carga_full
+from modules.cnpj_mask.janela import abrir_janela_cnpj_mask
 from modules.utils.janela import centralizar_janela
 
 
@@ -16,6 +17,7 @@ layout = QVBoxLayout()
 
 janela_carga = None
 janela_ativacao = None
+janela_mask = None
 
 
 def abrir_carga():
@@ -26,15 +28,23 @@ def abrir_ativacao():
     global janela_ativacao
     janela_ativacao = abrir_janela_ativacao()
 
+def abrir_mask():
+    global janela_mask
+    janela_mask = abrir_janela_cnpj_mask()
+
 botao_gerar = QPushButton("Gerar Email Carga FULL SGA")
 botao_gerar.setFixedSize(300, 150)
 botao_gerar.clicked.connect(abrir_carga)
 botao_validar = QPushButton("Validar componentes PETROMAX")
 botao_validar.setFixedSize(300, 150)
 botao_validar.clicked.connect(abrir_ativacao)
+botao_mask = QPushButton("Editar CNPJ")
+botao_mask.setFixedSize(300, 150)
+botao_mask.clicked.connect(abrir_mask)
 
 layout.addWidget(botao_gerar)
 layout.addWidget(botao_validar)
+layout.addWidget(botao_mask)
 
 janela.setLayout(layout)
 janela.show()
