2d39178107582259d3bdf015223940c33950cd92
Antony Novais Nunes <159445979+ognovais@users.noreply.github.com>
2026-07-30 08:27:44 -0300

    Add CNPJ mask editing window
    Implement a GUI for CNPJ masking and unmasking.

---
+++ modules/cnpj_mask/janela.py
@@ -0,0 +1,41 @@
+from PySide6.QtWidgets import QApplication, QLineEdit, QWidget, QVBoxLayout, QPushButton
+from modules.cnpj_mask.service import desmascarar_cnpj, mascarar_cnpj
+from modules.utils.janela import centralizar_janela
+
+def abrir_janela_cnpj_mask():
+    janela = QWidget()
+    janela.setWindowTitle("Editar CNPJ")
+    janela.resize(350, 200)
+    centralizar_janela(janela)
+
+    layout = QVBoxLayout()
+
+    campo_texto = QLineEdit()
+    campo_texto.setPlaceholderText("Cole aqui o CNPJ para ser editado")
+
+    campo_resultado = QLineEdit()
+    campo_resultado.setReadOnly(True)
+
+    def ao_clicar_mascarar():
+        campo_resultado.setText(mascarar_cnpj(campo_texto.text()))
+
+    def ao_clicar_desmascarar():
+        campo_resultado.setText(desmascarar_cnpj(campo_texto.text()))
+
+    botao_mascarar = QPushButton("Mascarar")
+    botao_mascarar.clicked.connect(ao_clicar_mascarar)
+    botao_mascarar.setFixedHeight(75)
+
+    botao_desmascarar = QPushButton("Desmascarar")
+    botao_desmascarar.clicked.connect(ao_clicar_desmascarar)
+    botao_desmascarar.setFixedHeight(75)
+
+    layout.addWidget(campo_texto)
+    layout.addWidget(botao_mascarar)
+    layout.addWidget(botao_desmascarar)
+    layout.addWidget(campo_resultado)
+
+    janela.setLayout(layout)
+    janela.show()
+
+    return janela
